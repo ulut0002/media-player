928ce651b6db5556140917f4043e453839629c68
Serdar Ulutas <ulut0002@algonquinlive.com>
2023-02-10 12:59:01 -0500

    Create APP class and move the code from DOMContentLoaded event to APP class. Also change image and media paths so it could be contained in APP class instead of data.js file

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
 import { data } from "./data.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const playerContainer = document.getElementById("player-container");
-  if (playerContainer) {
-    const playerEl = document.createElement("ulut0002-player");
-
-    playerEl.tracks = JSON.stringify(data);
-    playerEl.imagePath = data.imagePath;
-    playerEl.mediaPath = data.mediaPath;
-    playerContainer.append(playerEl);
-  }
+  APP.init();
 });
+
+const APP = {
+  playerContainer: null,
+  PATHS: {
+    IMAGE_PATH: "./img/",
+    MEDIA_PATH: "./media/",
+  },
+  init: function () {
+    APP.playerContainer = document.getElementById("player-container");
+    if (APP.playerContainer) {
+      const playerEl = document.createElement("ulut0002-player");
+
+      playerEl.tracks = JSON.stringify(data);
+      playerEl.imagePath = APP.PATHS.IMAGE_PATH;
+      playerEl.mediaPath = APP.PATHS.MEDIA_PATH;
+      APP.playerContainer.append(playerEl);
+    }
+  },
+};
